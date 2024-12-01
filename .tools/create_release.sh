da437c1e7ae010e021fde5bdce08a353090a6c6c
Miller-du <official.shutu@gmail.com>
2024-12-01 21:15:57 +0800

    update tools

--- .tools/create_release.sh
+++ .tools/create_release.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 ARCHIVE_NAME="release_$(date +%Y%m%d%H%M%S).zip"
-RELEASE_DIR="./release"
+RELEASE_DIR="./release/"
 REAL_PATH="$(realpath "$RELEASE_DIR")/$ARCHIVE_NAME"
 
 TEMP_DIR=$(mktemp -d)
-rsync -av --exclude-from=.gitignore --exclude=".git" --exclude=".tools" ./ "$TEMP_DIR/"
-
+rsync -av --exclude-from=./.tools/release_ignore.txt ./ "$TEMP_DIR/"
 cd "$TEMP_DIR"
 zip -r "$REAL_PATH" ./*
-cd -
 
 rm -rf "$TEMP_DIR"
 
-echo "Archive created at $RELEASE_DIR/$ARCHIVE_NAME"
+echo "Archive created at $(echo "$RELEASE_DIR/$ARCHIVE_NAME" | awk '{gsub(/\/+/, "/"); print}')"
